af5b45d6a80d61fffcb47e85a916d0f8a0e2bb99
Gabe Moothart <gmoothart@gmail.com>
2012-03-20 16:10:40 -0700

    drawing on canvas

--- mandelbrot.js
+++ mandelbrot.js
@@ -1,5 +1,21 @@
 
+Mandelbrot = {};
 (function() {
     "use strict";
+
+    Mandelbrot.draw = function(canvasEl) {
+        var w = canvasEl.width,
+            h = canvasEl.height,
+            x,y,
+            ctx = canvasEl.getContext('2d');
+
+        for(x=0; x<w; x++) {
+            for(y=0; y<h; y++) {
+                ctx.fillRect(x,y,1,1);
+                
+
+            }
+        }
+    }
 
 })();
